8ccfaa3e55f52e19be24750238c2ad71da01655e
DorothyMatlou <128805750+DorothyMatlou@users.noreply.github.com>
2023-12-05 09:12:01 +0200

    Created the project HTML structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import Weather from "./Weather.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div classname="Container">
+        <Weather />
+        <footer>
+          Coded by Dorothy Maleka and it is{" "}
+          <a
+            href="https://github.com/DorothyMatlou/reactapp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter City..."
+              className="form-control"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h2>Pretoria</h2>
+      <ul>
+        <li>Wednesday 07:00 AM</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+            alt="partly cloudy icon"
+          />
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 15%</li>
+            <li>Humidity: 75%</li>
+            <li>Wind-Speed: 2.5km/hr</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
