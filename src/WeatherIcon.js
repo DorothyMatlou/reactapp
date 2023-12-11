fae7dab8ca4773e5c8b9cb99cf8779600e998136
DorothyMatlou <128805750+DorothyMatlou@users.noreply.github.com>
2023-12-11 10:42:45 +0200

    Created weather forecast component to take care of the forecast

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
       color="rgb(33, 33, 33)"
-      size={50}
+      size={props.size}
       animate={true}
     />
   );
