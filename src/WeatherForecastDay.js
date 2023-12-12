5799fc2f3e01bfd31925a3abe998fe989f6854ff
DorothyMatlou <128805750+DorothyMatlou@users.noreply.github.com>
2023-12-12 16:03:17 +0200

    Created Weather forecast day component to take care of api response for forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -2,9 +2,16 @@ import React from "react";
 import WeatherIcon from "./WeatherIcon.js";
 
 export default function WeatherforecastDay(props) {
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
   return (
     <div>
-      <div className="WeatherForecastDay">{props.data.dt}</div>
+      <div className="WeatherForecastDay">{day()}</div>
       <WeatherIcon code={props.data.weather[0].icon} size={36} />
       <div className="WeatherForecast-temperatures">
         <span className="WeatherForecast-temperatures-max">
